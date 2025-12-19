33f155f04ae10d4f82785804aa78ec2b4adbf947
neotech-emanuel-juric <emanuel.juric@neo-tech-solutions.de>
2025-12-20 00:57:12 +0100

    add: count of every word in text

--- main.py
+++ main.py
@@ -4,9 +4,19 @@ def get_book_text(filepath: str) -> str:
     return content
 
 
+def count_words(text: str) -> int:
+    count = 0
+    txt = text.split()  # This has to be done, so that words are split from the string. Otherwise I would have counted every char
+
+    for word in txt:
+        count += 1
+    return count
+
+
 def main():
     text = get_book_text("books/frankenstein.txt")
-    print(text)
+    count = count_words(text)
+    print(f"Found {count} total words")
 
 
 if __name__ == "__main__":
